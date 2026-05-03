b18086b4cbbdf99f0b6c87ccda059adf2dd9a3f3
Indra N. Utomo <Indra77ya@gmail.com>
2026-05-03 15:53:31 +0700

    Benerin tulisan, layout dan tata letak

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.app')
 
 @section('title', 'Manajemen Lokasi')
-@section('page_title', 'Manajemen Lokasi')
+@section('page_title', '')
 
 @section('content')
-<div class="row row-cards">
-    <div class="col-12">
-        @livewire('location-manager')
+<div class="container-xl">
+    <div class="page-header d-print-none mb-3">
+        <div class="row align-items-center">
+            <div class="col">
+                <div class="page-pretitle">
+                    Master Data
+                </div>
+                <h2 class="page-title">
+                    Manajemen Lokasi
+                </h2>
+            </div>
+        </div>
     </div>
+
+    @livewire('location-manager')
 </div>
 @endsection
--- resources/views/rules/index.blade.php
+++ resources/views/rules/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+@section('title', 'Manajemen Peraturan')
 @section('page_title', '')
 
 @push('scripts')
