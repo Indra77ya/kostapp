9e571e315795f43e484d921c418607a7fbc5cec9
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-08-27 07:15:23 +0000

    Update Transfer Dana header title

--- resources/views/livewire/fund-transfer-manager.blade.php
+++ resources/views/livewire/fund-transfer-manager.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="row mb-3 align-items-center">
         <div class="col">
-            <h2 class="page-title">Transfer Dana (Kas & Bank)</h2>
+            <h2 class="page-title">Transfer Dana</h2>
         </div>
         <div class="col-auto ms-auto d-print-none">
             <button class="btn btn-primary" wire:click="openModal">
