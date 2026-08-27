b6385e5d63ae483a27702a94b307aaab558e2af5
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-08-27 07:32:35 +0000

    Add Fund Transfer feature with automatic journal postings and admin fee tracking

--- resources/views/livewire/fund-transfer-manager.blade.php
+++ resources/views/livewire/fund-transfer-manager.blade.php
@@ -105,7 +105,7 @@
     <!-- Create Transfer Modal -->
     @if($isModalOpen)
         <div class="modal modal-blur fade show d-block" tabindex="-1" style="background-color: rgba(0,0,0,0.5);">
-            <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">Tambah Transfer Dana</h5>
