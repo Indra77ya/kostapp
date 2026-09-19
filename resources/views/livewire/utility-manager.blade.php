80240e88229d0982cd01dbae9d64c5f7a1b77fcb
Indra77ya <42796139+Indra77ya@users.noreply.github.com>
2026-09-19 03:00:51 +0000

    style: Use Tabler printer icon button for utility receipt and invoice print actions
    Updated the utility meter readings table actions in utility-manager.blade.php to render icon-only Tabler printer SVG buttons (`btn-icon`) with tooltips for receipt and invoice printing per user request.

--- resources/views/livewire/utility-manager.blade.php
+++ resources/views/livewire/utility-manager.blade.php
@@ -154,12 +154,12 @@
                                             @endphp
 
                                             @if($latestPayment)
-                                                <a href="{{ route('payments.invoice', $latestPayment->id) }}" target="_blank" class="btn btn-white btn-sm text-primary" title="Cetak Kuitansi Pembayaran">
-                                                    Kuitansi
+                                                <a href="{{ route('payments.invoice', $latestPayment->id) }}" target="_blank" class="btn btn-white btn-icon btn-sm text-primary" title="Cetak Kuitansi Pembayaran" data-bs-toggle="tooltip">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-printer" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" /><path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" /><path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" /></svg>
                                                 </a>
                                             @else
-                                                <a href="{{ route('bills.invoice', $item->bill->id) }}" target="_blank" class="btn btn-white btn-sm text-secondary" title="Cetak Invoice Tagihan">
-                                                    Invoice
+                                                <a href="{{ route('bills.invoice', $item->bill->id) }}" target="_blank" class="btn btn-white btn-icon btn-sm text-secondary" title="Cetak Invoice Tagihan" data-bs-toggle="tooltip">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-printer" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" /><path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" /><path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" /></svg>
                                                 </a>
                                             @endif
                                         @elseif($item->registration_id)
